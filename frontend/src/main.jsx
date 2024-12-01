d5517e041b08696e527b665591247f0811599112
Narotam-Mishra <narotam21jan@gmail.com>
2024-12-01 22:13:56 +0530

    full-stack-blog-app-react-router-setup_1.5

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+import HomePage from './routes/HomePage.jsx';
+import PostListPage from './routes/PostListPage.jsx';
+import Write from './routes/Write.jsx';
+import LoginPage from './routes/LoginPage.jsx';
+import RegisterPage from './routes/RegisterPage.jsx';
+import SinglePostPage from './routes/SinglePostPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/posts",
+    element: <PostListPage />,
+  },
+  {
+    path: "/:slug",
+    element: <SinglePostPage />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
