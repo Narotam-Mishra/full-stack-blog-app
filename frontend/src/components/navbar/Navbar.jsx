56df808d1f7f7eede1d34770b770b4b969132b9b
Narotam-Mishra <narotam21jan@gmail.com>
2024-12-19 23:18:53 +0530

    full-stack-blog-app-added_react-router-dom-outlet_1.6

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Image from "../Image";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* Logo */}
-      <div className="flex items-center gap-4 text-2xl font-bold">
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Image
           src="logo.png"
           alt="Logo_image"
@@ -14,7 +15,7 @@ const Navbar = () => {
           h = {32}
         />
         <span>mylog</span>
-      </div>
+      </Link>
 
       {/* Mobile Menu */}
       <div className="md:hidden">
@@ -31,29 +32,29 @@ const Navbar = () => {
             open ? "-right-0" : "-right-[100%]"
           }`}
         >
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Popular</a>
-          <a href="/">About</a>
-          <a href="">
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
               Login 👋
             </button>
-          </a>
+          </Link>
         </div>
       </div>
 
       {/* Desktop Menu */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="/">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
             Login 👋
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
